d8fbc8ef7bb424169352096ab0b1c9a1183f02aa
J.S.Rishi Varma <165695619+JSRISHIVARMA@users.noreply.github.com>
2026-05-28 17:59:01 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,21 @@
+a = 10
+print("Type of a is : ",type(a))
+b=2.1
+print("Type of b is : ",type(b))
+c = 122.22j
+print("Type of c is : ",type(c))
+d = "Vamsi"
+print("Type of d is : ",type(d))
+e = ['tanuri','vamsi']
+print("Type of e is : ",type(e))
+f = ('1','Vamsi')
+print("Type of f is : ",type(f))
+g = {1212,"Vamsi","212.3212"}
+print("Type of g is : ",type(g))
+h = {1:"vamsi",2:"Rishi"}
+print("Type of h is : ",type(h))
+i = True
+j = False
+print("Type of i and j is : ",type(i))
+k = None
+print("Type of k is : ",type(k))
