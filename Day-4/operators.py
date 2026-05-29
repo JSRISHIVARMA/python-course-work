ef3ad0d3c27d09c127551d188e10a94eb4ea3f24
J.S.Rishi Varma <165695619+JSRISHIVARMA@users.noreply.github.com>
2026-05-29 17:53:33 +0530

    Add files via upload

---
+++ Day-4/operators.py
@@ -0,0 +1,53 @@
+# Arithmetic Operators
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
+
+# Comparison Operators
+a = 10
+b = 5
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+# Assignment Operators
+print("\nAssignment Operators")
+x = 10
+x += 5
+print("x += 5 :", x)
+
+# Logical Operators
+print("\nLogical Operators")
+print(True and False)
+print(True or False)
+print(not True)
+
+# Bitwise Operators
+print("\nBitwise Operators")
+print("a & b =", a & b)
+print("a | b =", a | b)
+
+# Membership Operators
+print("\nMembership Operators")
+list1 = [1, 2, 3, 4]
+print(2 in list1)
+print(5 not in list1)
+
+# Identity Operators
+print("\nIdentity Operators")
+m = [1, 2, 3]
+n = m
+print(m is n)
+print(m is not n)
+
